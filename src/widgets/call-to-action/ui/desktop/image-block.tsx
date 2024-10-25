4e8f5a2c9507c287e386f8c3a15df3e45ddc4b4f
met43211 <matvey-anashkin@yandex.ru>
2024-10-25 15:24:48 +0300

    fix: remove fragment

--- src/widgets/call-to-action/ui/desktop/image-block.tsx
+++ src/widgets/call-to-action/ui/desktop/image-block.tsx
@@ -5,15 +5,13 @@ import SButton from '@/shared/assets/images/s_button.png';
 
 export const DesktopImageBlock = () => {
   return (
-    <>
-      <View centered className='relative h-full'>
-        <Picture
-          alt='button'
-          className='absolute left-0 -ml-20 -mt-10 -translate-y-1/4'
-          src={SButton}
-        />
-        <Circles className='absolute bottom-0 top-0 -z-20 mt-20 -translate-x-1/4' />
-      </View>
-    </>
+    <View centered className='relative h-full'>
+      <Picture
+        alt='button'
+        className='absolute left-0 -ml-20 -mt-10 -translate-y-1/4'
+        src={SButton}
+      />
+      <Circles className='absolute bottom-0 top-0 -z-20 mt-20 -translate-x-1/4' />
+    </View>
   );
 };
