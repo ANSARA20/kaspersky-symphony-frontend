aa3fb59fec1160d2dd2c9ea23f9667bcf9b191ce
met43211 <matvey-anashkin@yandex.ru>
2024-12-05 11:46:50 +0300

    fix: delete auth key

--- src/entities/results/api/fetch-prize.ts
+++ src/entities/results/api/fetch-prize.ts
@@ -1,11 +1,7 @@
 export const fetchPrize = async () => {
   const url = process.env.API_URL;
-  const authKey = process.env.AUTH_KEY;
   const response = await fetch(`${url}/user`, {
     method: 'POST',
-    headers: {
-      'Auth-Key': authKey,
-    },
   });
 
   return response.json();
--- src/widgets/leaders/api/fetch-leaders.tsx
+++ src/widgets/leaders/api/fetch-leaders.tsx
@@ -1,11 +1,6 @@
 export const fetchLeaders = async () => {
   const url = process.env.API_URL;
-  const authKey = process.env.AUTH_KEY;
-  const response = await fetch(`${url}/users/leaders`, {
-    headers: {
-      'Auth-Key': authKey,
-    },
-  });
+  const response = await fetch(`${url}/users/leaders`);
 
   return response.json();
 };
