cf522a677bc69011ee0073d62f6f6aa4a0b5a74a
met43211 <matvey-anashkin@yandex.ru>
2024-12-18 18:48:25 +0300

    fix: phone format in leaders

--- src/widgets/leaders/config/leaders-example.ts
+++ src/widgets/leaders/config/leaders-example.ts
@@ -4,31 +4,31 @@ export const LeadersExample = [
   {
     id: 9999900,
     name: 'x Иван x',
-    phone: '+7 (985) ***-**-**',
+    phone: '+7 (926) ***-**-34',
     prizeId: EPrizeIds.BROOM,
   },
   {
     id: 9999901,
     name: 'x Сергей x',
-    phone: '+7 (985) ***-**-**',
+    phone: '+7 (916) ***-**-22',
     prizeId: EPrizeIds.HOODIE,
   },
   {
     id: 9999902,
     name: 'x Наталья x',
-    phone: '+7 (985) ***-**-**',
+    phone: '+7 (985) ***-**-12',
     prizeId: EPrizeIds.CAP,
   },
   {
     id: 9999903,
     name: 'x Петр x',
-    phone: '+7 (985) ***-**-**',
+    phone: '+7 (985) ***-**-65',
     prizeId: EPrizeIds.CLEANING_KIT,
   },
   {
     id: 9999904,
     name: 'x Анна x',
-    phone: '+7 (985) ***-**-**',
+    phone: '+7 (916) ***-**-84',
     prizeId: EPrizeIds.PHONE_STAND,
   },
 ];
